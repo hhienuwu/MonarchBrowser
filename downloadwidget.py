08a2375951ba3d67a6bf902098e984f3d4fcb552
hhienuwu <108988263+hhienuwu@users.noreply.github.com>
2024-01-07 20:44:12 +0700

    Add files via upload

---
+++ downloadwidget.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from ui_downloadwidget import Ui_DownloadWidget
+
+from PySide6.QtWebEngineCore import QWebEngineDownloadRequest
+from PySide6.QtWidgets import QFrame, QWidget
+from PySide6.QtGui import QIcon
+from PySide6.QtCore import QElapsedTimer, Signal, Slot
+
+def with_unit(bytes):
+    if bytes < (1 << 10):
+        return f"{bytes} Byte(s)"
+    if bytes < (1 << 20):
+        s = bytes / (1 << 10)
+        return f"{int(s)} KB"
+    if bytes < (1 << 30):
+        s = bytes / (1 << 20)
+        return f"{int(s)} MB"
+    s = bytes / (1 << 30)
+    return f"{int(s)} GB"
+
+
+class DownloadWidget(QFrame):
+    remove_clicked = Signal(QWidget)
+
+    def __init__(self, download, parent=None):
+        super().__init__(parent)
+        self._download = download
+        self._time_added = QElapsedTimer()
+        self._time_added.start()
+        self._cancel_icon = QIcon(".\\data\\stop.png")
+        self._remove_icon = QIcon(".\\data\\clear.png")
+
+        self._ui = Ui_DownloadWidget()
+        self._ui.setupUi(self)
+        self._ui.m_dstName.setText(self._download.downloadFileName())
+        self._ui.m_srcUrl.setText(self._download.url().toDisplayString())
+
+        self._ui.m_cancelButton.clicked.connect(self._canceled)
+
+        self._download.totalBytesChanged.connect(self.update_widget)
+        self._download.receivedBytesChanged.connect(self.update_widget)
+
+        self._download.stateChanged.connect(self.update_widget)
+
+        self.update_widget()
+
+    @Slot()
+    def _canceled(self):
+        state = self._download.state()
+        if state == QWebEngineDownloadRequest.DownloadInProgress:
+            self._download.cancel()
+        else:
+            self.remove_clicked.emit(self)
+
+    def update_widget(self):
+        total_bytes_v = self._download.totalBytes()
+        total_bytes = with_unit(total_bytes_v)
+        received_bytes_v = self._download.receivedBytes()
+        received_bytes = with_unit(received_bytes_v)
+        elapsed = self._time_added.elapsed()
+        bytes_per_second_v = received_bytes_v / elapsed * 1000 if elapsed else 0
+        bytes_per_second = with_unit(bytes_per_second_v)
+
+        state = self._download.state()
+
+        progress_bar = self._ui.m_progressBar
+        if state == QWebEngineDownloadRequest.DownloadInProgress:
+            if total_bytes_v > 0:
+                progress = round(100 * received_bytes_v / total_bytes_v)
+                progress_bar.setValue(progress)
+                progress_bar.setDisabled(False)
+                fmt = f"%p% - {received_bytes} trên {total_bytes} đã tải - {bytes_per_second}/s"
+                progress_bar.setFormat(fmt)
+            else:
+                progress_bar.setValue(0)
+                progress_bar.setDisabled(False)
+                fmt = f"Kích thước không xác định - {received_bytes} đã tải - {bytes_per_second}/s"
+                progress_bar.setFormat(fmt)
+        elif state == QWebEngineDownloadRequest.DownloadCompleted:
+            progress_bar.setValue(100)
+            progress_bar.setDisabled(True)
+            fmt = f"Hoàn thành - {received_bytes} đã tải - {bytes_per_second}/s"
+            progress_bar.setFormat(fmt)
+        elif state == QWebEngineDownloadRequest.DownloadCancelled:
+            progress_bar.setValue(0)
+            progress_bar.setDisabled(True)
+            fmt = f"Đã hủy - {received_bytes} đã tải - {bytes_per_second}/s"
+            progress_bar.setFormat(fmt)
+        elif state == QWebEngineDownloadRequest.DownloadInterrupted:
+            progress_bar.setValue(0)
+            progress_bar.setDisabled(True)
+            fmt = "Bị gián đoạn: " + self._download.interruptReasonString()
+            progress_bar.setFormat(fmt)
+
+        if state == QWebEngineDownloadRequest.DownloadInProgress:
+            self._ui.m_cancelButton.setIcon(self._cancel_icon)
+            self._ui.m_cancelButton.setToolTip("Dừng download")
+        else:
+            self._ui.m_cancelButton.setIcon(self._remove_icon)
+            self._ui.m_cancelButton.setToolTip("Xóa khỏi danh sách")
---
+++ ui_certificateerrordialog.py
@@ -0,0 +1,75 @@
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractButton, QApplication, QDialog, QDialogButtonBox,
+    QLabel, QSizePolicy, QSpacerItem, QVBoxLayout,
+    QWidget)
+
+class Ui_CertificateErrorDialog(object):
+    def setupUi(self, CertificateErrorDialog):
+        if not CertificateErrorDialog.objectName():
+            CertificateErrorDialog.setObjectName(u"Chứng chỉ lỗi")
+        CertificateErrorDialog.resize(370, 141)
+        self.verticalLayout = QVBoxLayout(CertificateErrorDialog)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(20, -1, 20, -1)
+        self.m_iconLabel = QLabel(CertificateErrorDialog)
+        self.m_iconLabel.setObjectName(u"m_iconLabel")
+        self.m_iconLabel.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout.addWidget(self.m_iconLabel)
+
+        self.m_errorLabel = QLabel(CertificateErrorDialog)
+        self.m_errorLabel.setObjectName(u"m_errorLabel")
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.m_errorLabel.sizePolicy().hasHeightForWidth())
+        self.m_errorLabel.setSizePolicy(sizePolicy)
+        self.m_errorLabel.setAlignment(Qt.AlignCenter)
+        self.m_errorLabel.setWordWrap(True)
+
+        self.verticalLayout.addWidget(self.m_errorLabel)
+
+        self.m_infoLabel = QLabel(CertificateErrorDialog)
+        self.m_infoLabel.setObjectName(u"m_infoLabel")
+        sizePolicy1 = QSizePolicy(QSizePolicy.MinimumExpanding, QSizePolicy.MinimumExpanding)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.m_infoLabel.sizePolicy().hasHeightForWidth())
+        self.m_infoLabel.setSizePolicy(sizePolicy1)
+        self.m_infoLabel.setAlignment(Qt.AlignLeading|Qt.AlignLeft|Qt.AlignVCenter)
+        self.m_infoLabel.setWordWrap(True)
+
+        self.verticalLayout.addWidget(self.m_infoLabel)
+
+        self.verticalSpacer = QSpacerItem(20, 16, QSizePolicy.Minimum, QSizePolicy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.buttonBox = QDialogButtonBox(CertificateErrorDialog)
+        self.buttonBox.setObjectName(u"buttonBox")
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.No|QDialogButtonBox.Yes)
+
+        self.verticalLayout.addWidget(self.buttonBox)
+
+
+        self.retranslateUi(CertificateErrorDialog)
+        self.buttonBox.accepted.connect(CertificateErrorDialog.accept)
+        self.buttonBox.rejected.connect(CertificateErrorDialog.reject)
+
+        QMetaObject.connectSlotsByName(CertificateErrorDialog)
+
+    def retranslateUi(self, CertificateErrorDialog):
+        CertificateErrorDialog.setWindowTitle(QCoreApplication.translate("Chứng chỉ lỗi", u"Dialog", None))
+        self.m_iconLabel.setText(QCoreApplication.translate("Chứng chỉ lỗi", u"Icon", None))
+        self.m_errorLabel.setText(QCoreApplication.translate("Chứng chỉ lỗi", u"Lỗi", None))
+        self.m_infoLabel.setText(QCoreApplication.translate("Chứng chỉ lỗi", u"Nếu bạn muốn, bạn có thể truy cập vào trang chứng chỉ lỗi này\n"
+"\n"
+"Bạn có muốn vượt qua chứng chỉ lỗi và tiếp tục ?   ", None))
+
